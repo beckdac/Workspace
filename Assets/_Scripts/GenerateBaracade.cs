f206218a28a7a050d113cb3dbeef2629b2378e11
Dave Beck <beck.dac@live.com>
2018-03-30 23:55:10 -0700

    Newarly complete baracade generator

--- Assets/_Scripts/GenerateBaracade.cs
+++ Assets/_Scripts/GenerateBaracade.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 using UnityEditor;
 
-public class GenerateBaracade : MonoBehaviour {
+public class GenerateBaracade : MonoBehaviour
+{
 
     public GameObject floor;
     public GameObject baracadePrefab;
@@ -28,32 +29,10 @@ public class GenerateBaracade : MonoBehaviour {
         Vector3 baracadeSize = baracadeMeshRenderer.bounds.size;
         Debug.Log("The baracade has dimensions: " + baracadeSize);
 
-        // Determine how many copies to put on each axis
-        // Note we subtract the width of the baracade from the floor dimension after multiplying it by two
-        // to accomodate for the two copies coming into the corner from the other axis
-        int xCount = Mathf.FloorToInt((floorSize.x - baracadeSize.z * 2f) / (baracadeSize.x + baracadeSpacing));
-        int zCount = Mathf.FloorToInt((floorSize.z - baracadeSize.z * 2f) / (baracadeSize.x + baracadeSpacing));
-        Debug.Log("Placing " + xCount.ToString() + " copies on the x axis and " + zCount.ToString() + " copies on the z axis");
+        // generate the baracades
+        generateBaracadeOnAxis(0, 2, 1, floorSize, baracadeSize, baracadeParent);
+        generateBaracadeOnAxis(2, 0, 1, floorSize, baracadeSize, baracadeParent);
 
-        // Loop over locations and instantiate
-        GameObject newBaracade;
-        for (int i = 0; i < xCount; ++i)
-        {
-            for (int j = -1; j <= -1; j+=2)
-            {
-                newBaracade = PrefabUtility.InstantiatePrefab(baracadePrefab) as GameObject;
-                // Set the parent for the new baracade to be this object
-                newBaracade.transform.SetParent(baracadeParent.transform);
-                newBaracade.transform.localPosition = new Vector3(floorSize.x / 2f - baracadeSize.z + baracadeSize.x / 2f + 
-                    (float)i * baracadeSize.x, 0f - floorSize.y / 2f, (float)j * floorSize.z / 2f + j * baracadeSize.z + baracadeSpacing / 2f);
-                Debug.Log("Placing baracade " + (i + 1) + " at position " + newBaracade.transform.localPosition + "relative to " + floor);
-            }
-        }
-        for (int i = 0; i < zCount; ++i)
-        {
-            // Set the parent for the new baracade to be this object
-         //   newBaracade.transform.SetParent(baracadeParent);
-        }
     }
 
     public void resetBaracade()
@@ -65,8 +44,51 @@ public class GenerateBaracade : MonoBehaviour {
         {
             if (childBaracade.gameObject == this.gameObject)
                 continue;
-            Debug.Log(childBaracade.gameObject.name);
+            //Debug.Log(childBaracade.gameObject.name);
             GameObject.DestroyImmediate(childBaracade.gameObject);
+        }
+    }
+
+    private void generateBaracadeOnAxis(int axis, int offAxis, int minorAxis, Vector3 floorSize, Vector3 baracadeSize, GameObject baracadeParent)
+    {
+        // Determine how many copies to put on each axis
+        // Note we subtract the width of the baracade from the floor dimension after multiplying it by two
+        // to accomodate for the two copies coming into the corner from the other axis
+        int count = Mathf.FloorToInt(floorSize[axis] / (baracadeSize.x + baracadeSpacing));
+        int spacers = count - 1;
+        float baracadeLength = count * baracadeSize.x + spacers * baracadeSpacing;
+        float sizeDiff = floorSize[axis] - baracadeLength;
+        float startEndGap = sizeDiff / 2f;
+
+        // dump our debug info
+        string axisString = axis == 0 ? "x" : (axis == 1 ? "y" : (axis == 2 ? "z" : axis.ToString()));
+        Debug.Log("Placing " + count + " copies on the " + axisString + " axis");
+        Debug.Log("Baracade is " + baracadeLength + " on the " + axisString + " axis");
+        Debug.Log("The " + axisString + " axis start and end gap distance is " + sizeDiff + " / 2 = " + startEndGap);
+
+        // Loop over locations and instantiate
+        GameObject newBaracade;
+        for (int i = 0; i < count; ++i)
+        {
+            float iOffset = -floorSize[axis] / 2f + startEndGap + i * (baracadeSize.x + baracadeSpacing);
+            for (int j = -1; j <= 1; j += 2)
+            {
+                newBaracade = PrefabUtility.InstantiatePrefab(baracadePrefab) as GameObject;
+                // Set the parent for the new baracade to be this object
+                newBaracade.transform.SetParent(baracadeParent.transform);
+                Vector3 position = Vector3.zero;
+                position[axis] = iOffset;
+                position[offAxis] = (float)j * floorSize[offAxis] / 2f - (float)j * baracadeSize.z;
+                newBaracade.transform.localPosition = position;
+                Debug.Log("Placed baracade " + (i + 1) + " at position " + newBaracade.transform.localPosition + "relative to " + floor);
+                // The baracade prefab is built along the x axis, if we are doing any other axis we need to rotate the baracade
+                // First, find the center of the object
+                Vector3 center = newBaracade.GetComponent<MeshRenderer>().bounds.center;
+                // rotate the object about an axis at that center of rotation
+                Vector3 rotationAxis = Vector3.zero;
+                rotationAxis[minorAxis] = 1f;
+                newBaracade.transform.RotateAround(center, rotationAxis, 90f);
+            }
         }
     }
 }
